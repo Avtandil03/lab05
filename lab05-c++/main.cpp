1af435211465f8dc5bc7904c77e80132e663551d
Avtandil <avtandilqazxswedc@gmail.com>
2022-12-02 15:20:02 +0300

    add dec_to_hex

--- lab05-c++/main.cpp
+++ lab05-c++/main.cpp
@@ -9,13 +9,47 @@
 #include <iomanip>
 
 
+
 using namespace std;
 
+
+string dec_to_hex(int dec) {
+    int hex = 0, hex1 = 0;
+    string hexStr;
+    for (; hex <= dec - 16; hex1++)
+    {
+        hex = hex + 16;
+    }
+    hex = dec - hex;
+    switch (hex)
+    {
+        case 10: hexStr = 'A';
+            break;
+        case 11: hexStr = 'B';
+            break;
+        case 12: hexStr = 'C';
+            break;
+        case 13: hexStr = 'D';
+            break;
+        case 14: hexStr = 'E';
+            break;
+        case 15: hexStr = 'F';
+            break;
+    default:
+        if (hex < 10)
+            hexStr = to_string(hex);
+        break;
+    }
+    return to_string(hex1) + hexStr;
+}
 
 
 int main(void)
 {
     setlocale(LC_ALL, "Russian");
-         
-    cout << endl;
+    int value;
+    cin >> value;
+    
+    cout << endl << dec_to_hex(value)<< endl;
+    system("PAUSE");
 }
