f4efdb741cce64b76543d3b39193964b291a6663
Avtandil <avtandilqazxswedc@gmail.com>
2022-12-06 16:04:18 +0300

    modify Student

--- lab05-c++/main.cpp
+++ lab05-c++/main.cpp
@@ -12,18 +12,39 @@
 
 using namespace std;
 
+struct Studetn
+{
+	char name[16] = "\0";
+	uint16_t year;
+	float avgScore;
+	bool sex;
+	uint8_t courseCount;
+	void fillInfo() {
+		while (strlen(this->name) < 2 )
+		{
+			cout << "Имя: ";
+			cin >> this->name;
+		}
+		while (this->year < 1950 || this->year > 2023)
+		{
+			cout << "Год поступления: ";
+			cin >> this->year;
+		}
+	};
+};
+
+
 int main(void)
 {
     setlocale(LC_ALL, "Russian");
+	
+	struct Studetn *starosta;
 
-	struct Studetn
-	{
-	char name[16] ;
+	struct Studetn students[3];
+	starosta = &students[0];
 
-	};
-
-	Studetn starosta;
-	cout<<strlen(starosta.name);
+	starosta->fillInfo();
+	
 
 	system("pause");
 
