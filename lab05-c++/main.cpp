a2482ba9b8a1d448af5f2bbf92d6f6a1e22d8879
Avtandil <avtandilqazxswedc@gmail.com>
2022-12-06 12:23:51 +0300

    modify main.cpp

---
+++ lab05-c++/main.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <conio.h>
+#include<string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+
+
+
+using namespace std;
+
+int main(void)
+{
+    setlocale(LC_ALL, "Russian");
+}
